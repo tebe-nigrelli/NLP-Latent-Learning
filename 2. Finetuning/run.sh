d4cadfac95f7c414ee600c4a69f16a472d89a1e2
Tebe Nigrelli <nigrellitebe@gmail.com>
2026-04-24 09:47:03 +0200

    fix: run.sh for hpc

--- 2. Finetuning/run.sh
+++ 2. Finetuning/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name="NLP-finetune"
 #SBATCH --account=3189081
-#SBATCH --partition=ai
+#SBATCH --partition=stud
 #SBATCH --mem=16G
 #SBATCH --cpus-per-task=1
 #SBATCH --gpus=1
@@ -10,9 +10,9 @@
 #SBATCH --mail-type=END
 #SBATCH --mail-user=3189081@studbocconi.it
 
-cd ~/NLP-Latent-Learning
+cd ~/NLP-Latent-Learning/2.\ Finetuning/
 
-module load modules/miniconda3
+source $(conda info --base)/etc/profile.d/conda.sh
 conda activate /home/3189081/.conda/envs/uv
 
-uv run efficient_b_train.py
+uv run python main.py
